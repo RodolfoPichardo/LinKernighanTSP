7a88a69f5abb5f237ebbeeab9cbe60f0f2d2a0ce
Rodolfo Pichardo <rodolfopichardomena@gmail.com>
2017-04-03 16:52:19 -0400

    Documented this class

--- src/Edge.java
+++ src/Edge.java
@@ -1,22 +1,54 @@
 import java.util.*;
 
+/**
+ * This class is meant for representing the edges, it allows to store
+ * the endpoints ids and compare the edges
+ */
 public class Edge implements Comparable<Edge> {
+    /*
+     * Instance variables
+     */
+
+    // The first node
     private int endPoint1;
+    
+    // The second node
     private int endPoint2;
     
+    /**
+     * Constructor that takes the two endpoints id
+     * @param int the id of the first endpoint
+     * @param int the id of the second endpoint
+     */
     public Edge(int a, int b) {
         this.endPoint1 = a > b? a:b;
         this.endPoint2 = a > b? b:a;
     }
 
+    /**
+     * Getter that returns the first endpoint id
+     * @param None
+     * @return the first endpoint id
+     */
     public int get1() {
         return this.endPoint1;
     }
 
+    /**
+     * Getter that returns the second endpoint id
+     * @param None
+     * @return the second endpoint id
+     */
     public int get2() {
         return this.endPoint2;
     }
 
+    /**
+     * Method that compares two edges, here to make this class Comparable
+     * @see java.util.Comparable
+     * @param Edge the edge that is going to be compared against this one
+     * @return int will return -1 if less, 0 if equal, and 1 if greater
+     */
     public int compareTo(Edge e2) {
         if(this.get1() < e2.get1() || this.get1() == e2.get1() && this.get2() < e2.get2()) {
             return -1;
@@ -28,6 +60,12 @@ public class Edge implements Comparable<Edge> {
 
     }
 
+    /**
+     * Method that compares two edges, here to make this class Comparable
+     * @see java.util.Comparable
+     * @param Edge the edge that is going to be compared against this one
+     * @return boolean true if both share the same endpoints, false otherwise
+     */
     public boolean equals(Edge e2) {
         return (this.get1() == e2.get1()) && (this.get2() == e2.get2());
     }
